3d726218246b468f2884092f6fc8cb10ad45b17f
Yan Campos <camposyan00@gmail.com>
2024-02-09 16:52:06 -0300

    ajuste

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { DefaultLayout } from "./layout/DefaultLayout";
-// import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home";
 import { Users } from "./pages/Users";
 import { Exercises } from "./pages/Exercises";
@@ -13,7 +12,6 @@ export const Router = createBrowserRouter(
                <Route path="/" element={<Login />} />
                <Route path="/" element={<DefaultLayout />}>
                     <Route path="home" element={<Home />} />
-                    {/* <Route path="profile" element={<Profile />} /> */}
                     <Route path="users" element={<Users />} />
                     <Route path="exercises" element={<Exercises />} />
                     <Route path="sheets" element={<Sheets />} />
--- src/pages/Sheets.tsx
+++ src/pages/Sheets.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "../components/Container";
 
 export function Sheets() {
      const [isLoading, setIsLoading] = useState<boolean>(false);
 
-     setIsLoading(false)
+     useEffect(() => {
+          setIsLoading(false)
+     }, [])
+
      return (
           <Container
                title={"Fichas"}
